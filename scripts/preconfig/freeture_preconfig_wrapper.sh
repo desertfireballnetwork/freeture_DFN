57663b220536cde4db04efc68b441f94cbbaa9ec
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2021-06-15 22:13:47 +0800

    fix creating preconfig subdir in /data0/log/freeture; some prints fixed

--- scripts/preconfig/freeture_preconfig_wrapper.sh
+++ scripts/preconfig/freeture_preconfig_wrapper.sh
@@ -2,7 +2,10 @@
 
 FREETURE_LOG_DIR="/data0/log/freeture"
 FREETURE_PRECONFIG_LOG_DIR=${FREETURE_LOG_DIR}/preconfig
-[ -d ${FREETURE_PRECONFIG_LOG_DIR} ] || mkdir -p ${FREETURE_PRECONFIG_LOG_DIR} 
+if [ ! -d ${FREETURE_PRECONFIG_LOG_DIR} ]
+then
+    mkdir -p ${FREETURE_PRECONFIG_LOG_DIR}
+fi
 FREETURE_PRECONFIG_LOG=${FREETURE_PRECONFIG_LOG_DIR}/freeture_preconfig_$(date -u "+%Y-%m-%d_%H%M%S")UTC.log
 
 FREETURE_DEAMON_CFG="/usr/local/etc/dfn/freeture.cfg"
@@ -21,13 +24,13 @@ preconfig()
 	freetureconf=$(tail -n 1 ${FREETURE_PRECONFIG_LOG})
 	echo "Generating config file OK as ${freetureconf}"
 	# dump it as default for future, if something went wrong
-	echo save as default freeture config file ${DEAMON_CFG}
+	echo save as default freeture config file ${FREETURE_DEAMON_CFG}
 	cp ${freetureconf} ${FREETURE_DEAMON_CFG}
     else
 	# running of python3 /opt/dfn-software/freeture_preconfig.py failed
 	# perhaps some internal interval_control_sw incompatibility
-	# let's go with default ${DEAMON_CFG}
-	echo "Generating config file FAILED, using previous one in file ${DEAMON_CFG}"
+	# let's go with default ${FREETURE_DEAMON_CFG}
+	echo "Generating config file FAILED, using previous one in file ${FREETURE_DEAMON_CFG}"
     fi
 }
 
